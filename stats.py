0f07490b7310f7369585a2a879a99e1166130ea4
AlexisBlossom <ajtauer00@gmail.com>
2025-05-26 06:38:14 -0400

    finalized version

---
+++ stats.py
@@ -0,0 +1,32 @@
+import sys
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+def arguments(a):
+    if len(a) < 2:
+        sys.exit(1,"Usage: python3 main.py <path_to_book>")
+    else:
+        return sys.argv[1]
